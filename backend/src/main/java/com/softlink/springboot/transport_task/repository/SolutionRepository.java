8adcb9896b5bcb1fc81c51019f987d44ac30e03e
QRWER <mum500678@gmail.com>
2025-06-26 16:01:38 +0400

    Rename

--- backend/src/main/java/com/softlink/springboot/transport_task/repository/SolutionRepository.java
+++ backend/src/main/java/com/softlink/springboot/transport_task/repository/SolutionRepository.java
@@ -3,5 +3,5 @@ package com.softlink.springboot.transport_task.repository;
 import com.softlink.springboot.transport_task.entity.Solution;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskSolutionRepository extends JpaRepository<Solution, Integer> {
+public interface SolutionRepository extends JpaRepository<Solution, Integer> {
 }
--- backend/src/main/java/com/softlink/springboot/transport_task/service/TaskSolutionService.java
+++ backend/src/main/java/com/softlink/springboot/transport_task/service/TaskSolutionService.java
@@ -1,9 +1,7 @@
 package com.softlink.springboot.transport_task.service;
 
 import com.softlink.springboot.transport_task.entity.Solution;
-import com.softlink.springboot.transport_task.entity.TransportTask;
-import com.softlink.springboot.transport_task.repository.TaskSolutionRepository;
-import com.softlink.springboot.transport_task.repository.TransportTaskRepository;
+import com.softlink.springboot.transport_task.repository.SolutionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,17 +14,17 @@ import java.util.Optional;
 public class TaskSolutionService {
 
     @Autowired
-    private TaskSolutionRepository taskSolutionRepository;
+    private SolutionRepository solutionRepository;
 
     public List<Solution> getAll(){
-        return taskSolutionRepository.findAll();
+        return solutionRepository.findAll();
     }
 
     public Optional<Solution> getById(int id){
-        return taskSolutionRepository.findById(id);
+        return solutionRepository.findById(id);
     }
 
     public Solution save(Solution solution){
-        return taskSolutionRepository.save(solution);
+        return solutionRepository.save(solution);
     }
 }
